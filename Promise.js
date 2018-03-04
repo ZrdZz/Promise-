800b6d0f2b889adb7201cf58c4c29d64c091213c
ZrdZz <17805009970@163.com>
2018-03-04 11:46:37 +0800

    串行Promise

--- Promise.js
+++ Promise.js
@@ -3,24 +3,43 @@ function Promise(fn){
       value,
       state = 'pending';
   
-  //添加state, 当state为pending时, 将其添加到队列中, 若不为说明这时Promise已经执行完, 直接执行回调函数
   this.then = function(onFulfilled){
+    return new Promise(function(resolve){
+        handle({
+            onFulfilled = null,
+            resolve
+        })
+    })
+  }
+  
+  function handle(callback){
     if(state === 'pending'){
-      callbacks.push(onFulfilled);
-      return
+        callbacks.push(callback);
+        return 
     }
     
-    onFulfilled(value);
+    if(!callback.onFulfilled){
+        callback.resolve(value);
+    }
+    
+    var ret = callback.onFulfilled(value);
+    callback.resolve(ret);
   }
   
   function resolve(newValue){
+    if(newValue && (typeof newValue === 'object' || typeof newValue === 'function')){
+        var then = newValue.then;
+        if(typeof then === 'function'){
+            then.call(newValue, resolve);
+            return;
+        }
+    }
+    
     value = newValue
     state = 'fulfilled';
     
-    //利用setTimeout将callbacks的执行添加到消息队列中, 这样当他执行时保证then已执行完
     setTimeout(function(){
       callbacks.forEach(function(onFulfilled){
-        //通过给value赋值, 可以将前一个callback的返回值赋给下一个callback
         value = onFulfilled(value)
       })
     }, 0)
@@ -28,5 +47,3 @@ function Promise(fn){
   
   fn(resolve)
 }
-
-改好了
